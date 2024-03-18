3ad2ec3b57ae28af8d149fb4f6d47c9935fc47f2
Augustin Malle <augustin.malle@gmail.com>
2024-03-18 12:58:20 +0100

    sitemap an some config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
+  prefetch: {
+    defaultStrategy: 'viewport'
+  }
 });

67b36b3d3ed905efa6e37acb91d3de51c00cc538
Augustin Malle <augustin.malle@gmail.com>
2024-05-30 12:36:02 +0200

    asd

--- src/lib/article.js
+++ src/lib/article.js
@@ -67,10 +67,33 @@ export async function getArticleBySlugAPI(slug) {
         
     var params = {
     };
-
+                                  
     const response = await fetch("https://sfsn.si/wp-json/nre/v1/getlastarticles/",
     {
       method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+        'Cache-Control': 'public, s-maxage=60,stale-while-revalidate=59',
+        'CDN-Cache-Control': 'public, s-maxage=60,stale-while-revalidate=59',
+        'Vercel-CDN-Cache-Control': 'public, s-maxage=60,stale-while-revalidate=59',
+      }
+    });
+
+
+    const avtors = await response.json();
+
+    return avtors;
+  }
+
+  export async function getRelatedArticles() {
+        
+    var params = {
+    };
+                   
+    const response = await fetch("https://sfsn.si/wp-json/nre/v1/getrelatedarticles/",
+    {
+      method: 'POST',
       headers: {
         "Content-Type": "application/json",
         // 'Content-Type': 'application/x-www-form-urlencoded',

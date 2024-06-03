1e1b0402f68a700e7a194d8acd70c90fc897afda
Augustin Malle <augustin.malle@gmail.com>
2024-06-03 21:16:51 +0200

    asd

--- src/lib/article.js
+++ src/lib/article.js
@@ -23,40 +23,7 @@ export async function getArticleBySlugAPI(slug) {
 
 
     const article = await response.json();
-    /**
-     *
-     *    id
-          title 
-          blocks {
-            name
-            attributesJSON
-            dynamicContent
-            originalContent
-            isDynamic
-          }
-          excerpt
-          content
-          featuredImage {
-            node {
-              link
-              mediaItemUrl
-              altText
-              description
-              title
-              srcSet(size: IMG33)
-              sourceUrl(size: IMG33)
-            }
-          }
-          author {
-            node {
-              slug
-              firstName
-              lastName
-            }
-          }
-     * 
-     * 
-     */
+    
     
     return article;
   }
@@ -132,4 +99,32 @@ export async function getArticleBySlugAPI(slug) {
     const avtors = await response.json();
 
     return avtors;
+  }
+
+
+
+  export async function getPostsByCategoryId(slug,count) {
+        
+    var params = {
+      slug: slug,
+      count: count
+  };
+                   
+    const response = await fetch("https://sfsn.si/wp-json/nre/v1/getpostsbycategoryid/",
+    {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+        'Cache-Control': 'public, s-maxage=60,stale-while-revalidate=59',
+        'CDN-Cache-Control': 'public, s-maxage=60,stale-while-revalidate=59',
+        'Vercel-CDN-Cache-Control': 'public, s-maxage=60,stale-while-revalidate=59',
+      },
+      body: JSON.stringify(params)
+    });
+
+
+    const posts = await response.json();
+
+    return posts;
   }
